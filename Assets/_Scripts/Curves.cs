d142c8370b8a1424762b7b34cb0d4209f3b6c165
小仔 <chris58530@gmail.com>
2022-12-19 20:07:11 +0800

    resteraunt set

---
+++ Assets/_Scripts/Curves.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Curves : MonoBehaviour
+{
+
+    public  AnimationCurve talentUnlock;
+    public  AnimationCurve talentBonus;
+    public  AnimationCurve monsterBoost;
+    public  AnimationCurve levelEarn;
+    
+   
+}
